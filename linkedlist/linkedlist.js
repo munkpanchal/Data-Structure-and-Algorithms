2964055d4d733bf767251a1bef9945afda9c0531
Mayank Panchal <mayank@screetract.com>
2023-01-23 03:30:20 +0530

    Singly linked list class created

---
+++ linkedlist/linkedlist.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+const obj = new LinkedList();
