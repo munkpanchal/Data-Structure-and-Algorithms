ba24aacffbe1fd843c9da78396db17f6a777678d
Mayank Panchal <mayank@screetract.com>
2023-01-23 03:30:40 +0530

    Push method added to linked list

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -11,6 +11,17 @@ class LinkedList {
         this.tail = null;
         this.length = 0;
     }
+    push(val) {
+        const newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            this.tail.next = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 const obj = new LinkedList();
