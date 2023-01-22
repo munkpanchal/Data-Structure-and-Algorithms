b79a766688b8506257e304319ef33d7cf79bb858
Mayank Panchal <mayank@screetract.com>
2023-01-23 04:16:46 +0530

    Add element at in start of the array

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -82,6 +82,23 @@ class LinkedList {
         return this;
     }
 
+    // Add element to the start of the array
+
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+
+    }
+
+
 
 }
 
