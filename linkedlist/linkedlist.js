e14f4409905ac572609cbbc4accd861926812719
Mayank Panchal <mayank@screetract.com>
2023-01-23 03:42:02 +0530

    Print all the elment of the linkedlist

--- linkedlist/linkedlist.js
+++ linkedlist/linkedlist.js
@@ -11,17 +11,37 @@ class LinkedList {
         this.tail = null;
         this.length = 0;
     }
+    // Push method of LinkedList
+
     push(val) {
-        const newNode = new Node(val);
+        let newNode = new Node(val);
         if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
             this.tail.next = newNode;
+            this.tail = newNode;
         }
         this.length++;
         return this;
     }
+
+
+    // Print all the element of LinkedList
+
+    print() {
+
+        let current = this.head;
+        const arr = new Array();
+
+        while (current) {
+            arr.push(current.val);
+            current = current.next;
+        }
+        return arr;
+    }
+
+
 }
 
 const obj = new LinkedList();
